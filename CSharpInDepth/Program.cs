1e32f12509535168a50919d265cdd95ee6e54d2f
angel.castorena <acastorena@medxoom.com>
2022-07-17 15:43:17 -0500

    Copying elements from one collection to another using generics

--- CSharpInDepth/Program.cs
+++ CSharpInDepth/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace MyApp
+namespace CSharpInDepth
 {
-    internal class Program
+    static class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var numbers = GetNumbers();
+            var firstTwoNumbers = CopyAtMost<int>(numbers, 2);
+            Console.WriteLine(firstTwoNumbers.Count);
+
+            var names = GetNames();
+            var fisrtTwoNames = CopyAtMost<string>(names, 2);
+            Console.WriteLine(fisrtTwoNames.Count);
+
+        }
+
+
+        private static List<T> CopyAtMost<T>(List<T> input, int maxElements)
+        {
+            return input.Take(maxElements).ToList();
+        }
+
+        private static List<int> GetNumbers()
+        {
+            return new List<int>() { 1, 2, 3123, 123, 123, 123, 123, 123 };
+        }
+
+        private static List<string> GetNames()
+        {
+            return new List<string>() { "name1", "name2", "name3", "name4", "name5", "name6"};
         }
     }
+
 }
